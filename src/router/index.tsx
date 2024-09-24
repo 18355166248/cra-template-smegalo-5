96f8c48cfe88bf0fe9e3baa44f42e099b931bef3
lang.jiang <lang.jiang@ximalaya.com>
2024-09-24 10:11:57 +0800

    feat: 优化重定向

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,6 +5,7 @@ import { AntdItemTypeWithRouter } from "./types";
 import { isArray, pick } from "lodash-es";
 import { routerConfig } from "./data";
 import { basePathUrl } from "@/constants/common.const";
+import { isDev } from '@/utils/env';
 
 // 过滤路由配置中的菜单属性
 function gerRouterParams(list: AntdItemTypeWithRouter[]) {
@@ -16,6 +17,12 @@ function gerRouterParams(list: AntdItemTypeWithRouter[]) {
   });
 }
 const routerParams = gerRouterParams(routerConfig);
+
+// 测试环境自动拼接重定向到basename
+if (isDev && window.location.pathname === "/") {
+  window.history.pushState(null, "", basePathUrl);
+}
+
 
 export const router = createBrowserRouter(
   [
